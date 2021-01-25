d3b7a73a2651697cb9d58711f485e75e69df3f3a
benji300 <benji300@web.de>
2021-01-26 00:04:21 +0100

    Improved implementation of helpers.ts

--- src/helpers.ts
+++ src/helpers.ts
@@ -60,19 +60,9 @@ export class NoteTabs {
    * Inserts handled tab at specified index.
    */
   private async insertAtIndex(index: number, tab: any) {
-    if (index < 0 || tab == null) return;
+    if (index < 0 || tab === undefined) return;
 
     this._tabs.splice(index, 0, tab);
-    await this.store();
-  }
-
-  /**
-   * Replaces tab at specified index with handled one.
-   */
-  private async replaceAtIndex(index: number, tab: any) {
-    if (index < 0 || tab == null) return;
-
-    this._tabs.splice(index, 1, tab);
     await this.store();
   }
 
@@ -103,38 +93,28 @@ export class NoteTabs {
    * Gets index of tab for note with handled id. -1 if not exist.
    */
   indexOf(noteId: string): number {
-    if (noteId == null) return;
-
-    for (let i: number = 0; i < this.length(); i++) {
-      if (this._tabs[i]['id'] === noteId) return i;
-    }
-    return -1;
+    return this._tabs.findIndex(x => x.id === noteId);
   }
 
   /**
    * Gets index of the temporary tab. -1 if not exist.
    */
   indexOfTemp(): number {
-    for (let i: number = 0; i < this.length(); i++) {
-      if (this._tabs[i]['type'] === NoteTabType.Temporary) return i;
-    }
-    return -1;
+    return this._tabs.findIndex(x => x.type === NoteTabType.Temporary);
   }
 
   /**
    * Gets a value whether the handled note has already a tab or not.
    */
   hasTab(noteId: string): boolean {
-    if (noteId == null) return;
-
-    return this.indexOf(noteId) < 0 ? false : true;
+    return (this._tabs.find(x => x.id === noteId) !== undefined);
   }
 
   /**
    * Adds note as new tab at the end.
    */
   async add(noteId: string, noteType: NoteTabType) {
-    if (noteId == null) return;
+    if (noteId === undefined || noteType === undefined) return;
 
     this._tabs.push({ id: noteId, type: noteType });
     await this.store();
@@ -147,8 +127,6 @@ export class NoteTabs {
     if (sourceIdx < 0 || sourceIdx >= this.length()) return;
     if (targetIdx < 0 || targetIdx >= this.length()) return;
 
-    // console.log(`moveWithIndex: ${sourceIdx} to ${targetIdx} with length = ${this.length()}`);
-
     const tab: any = this._tabs[sourceIdx];
     await this.delete(this.get(sourceIdx).id);
     await this.insertAtIndex((targetIdx == 0 ? 0 : targetIdx), tab);
@@ -159,8 +137,6 @@ export class NoteTabs {
    * Moves the tab of source note to the index of the target note.
    */
   async moveWithId(sourceId: string, targetId: string) {
-    if (targetId == null || sourceId == null) return;
-
     await this.moveWithIndex(this.indexOf(sourceId), this.indexOf(targetId));
   }
 
@@ -168,17 +144,18 @@ export class NoteTabs {
     * Changes type of the tab for the handled note.
     */
   async changeType(noteId: string, newType: NoteTabType) {
-    if (!this.hasTab(noteId)) return;
-
-    this._tabs[this.indexOf(noteId)].type = newType;
-    await this.store();
+    const index = this.indexOf(noteId);
+    if (index >= 0) {
+      this._tabs[index].type = newType;
+      await this.store();
+    }
   }
 
   /**
    * Replaces tab at specified index with handled one.
    */
   async replaceTemp(noteId: string) {
-    if (noteId == null) return;
+    if (noteId === undefined) return;
 
     const tempIdx: number = this.indexOfTemp();
     if (tempIdx >= 0) {
@@ -191,12 +168,11 @@ export class NoteTabs {
    * Removes tab on handled index.
    */
   async delete(noteId: string) {
-    if (noteId == null) return;
-
-    if (this.hasTab(noteId)) {
-      this._tabs.splice(this.indexOf(noteId), 1);
+    const index = this.indexOf(noteId);
+    if (index >= 0) {
+      this._tabs.splice(index, 1);
+      await this.store();
     }
-    await this.store();
   }
 
   /**
